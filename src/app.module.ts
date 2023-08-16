3070be5bfaf22ae73a9728f0d1f56ab125b9600c
yuvraj <yuvrajsab@protonmail.com>
2023-08-16 12:12:40 +0530

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [__dirname + '/**/*.entity.{js,ts}'],
+          synchronize: configService.get('SYNC_DB') === 'true',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
